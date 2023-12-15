6ac3cdc595b1a10b344a01ed4854dcf9a695a931
Nitesh Kishor Warik <nitesh.warik@marico.com>
2023-12-15 12:16:58 +0530

    Nitesh-Chat bot info Stateful class added in the project.

---
+++ lib/chat_bot_info.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class ChatBotInfo extends StatefulWidget {
+  const ChatBotInfo({super.key});
+
+  @override
+  State<ChatBotInfo> createState() => _ChatBotInfoState();
+}
+
+class _ChatBotInfoState extends State<ChatBotInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
